e939f305f666bffcb0c6ea5e00ee48c3c0feb310
Piyush <piyushklaptop99@gmail.com>
2024-02-05 22:57:14 +0530

    Add another package

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dbConnect = require('./config/dbConnect');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const app = express();
+
 require('dotenv').config();
 const authRouter = require('./routes/authRoute');
 const productRouter = require('./routes/productRoute');
